c230f41e533024e08ca00c411edb1358e405e466
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-06-06 20:55:03 +0500

    Update styler.py

--- FlaskApp_NameStyler/styler.py
+++ FlaskApp_NameStyler/styler.py
@@ -1,1 +1,20 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
 
+@app.route('/' , methods=['GET','POST'])
+def Home():
+  data = None
+  if request.method=='POST':
+    name = request.form.get('uname')
+    
+    color = request.form.get('uname1')
+    color1 = color.lower()
+    print(color1)
+    data= [name,color1]
+    
+    return render_template('index.html', data=data)
+  else:
+    pass
+  return render_template('index.html')
+if __name__ == '__main__':
+  app.run(debug=True)
